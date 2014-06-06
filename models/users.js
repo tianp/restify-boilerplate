18aa1972f8c0f1d587490c5eb714d6b0e65d85b5
Tian Permana <hello@tianpermana.com>
2014-06-06 19:33:38 +0700

    add create user method

--- models/users.js
+++ models/users.js
@@ -11,6 +11,33 @@
     _.extend( this, db.users )
   }
 
+  Users.prototype.create = function( params, done ){
+
+    var defaults = {
+      dateSubmitted: {
+        timestamp: moment().unix(),
+        iso: moment().toISOString()
+      }
+    }
+
+    var user = _.defaults( params, defaults )
+
+    var options = { w: 1 } // write concern
+
+    this.insert( user, options, function( error, result ) {
+
+      if( error ){
+
+        done( error ) // something happens
+
+        return
+      }
+
+      done( null, result[0] ) // return back the recorded user
+
+    })
+  }
+
   module.exports = Users
 
 })()
