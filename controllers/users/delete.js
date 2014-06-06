911146e416c91271b5e4adbbb4832f6ef0a06005
Tian Permana <hello@tianpermana.com>
2014-06-06 19:33:04 +0700

    Add basic module exports on users controllers

---
+++ controllers/users/delete.js
@@ -0,0 +1,12 @@
+(function(){
+
+  var _ = require('underscore'),
+    async = require('async'),
+    moment = require('moment'),
+    Models = require('./../../models')
+
+  module.exports = function( params, done ){
+
+  }
+
+})()
