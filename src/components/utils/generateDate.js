5b2e16d4a9b519b2826d13783ee6ca8bb5fe2737
Tian Permana <hello@tianpermana.com>
2015-01-08 22:56:46 +0700

    clean up

--- src/components/utils/generateDate.js
+++ src/components/utils/generateDate.js
@@ -1,17 +1,5 @@
 (function(){
 
-  /**
-    * Babybind API - /src/components/generateDate.js
-    *
-    * Copyright (c) 2015 Babybind, Inc - All Rights Reserved
-    *
-    * Unauthorized copying of this file, via any medium is strictly prohibited
-    * Proprietary and confidential
-    *
-    * Written by Tian Permana <hello@tianpermana.com>, January 2014
-    *
-    */
-
   var moment = require('moment')
 
   module.exports = function(){
--- src/components/utils/generateId.js
+++ src/components/utils/generateId.js
@@ -1,17 +1,5 @@
 (function(){
 
-  /**
-    * Babybind API - /src/components/generateId.js
-    *
-    * Copyright (c) 2015 Babybind, Inc - All Rights Reserved
-    *
-    * Unauthorized copying of this file, via any medium is strictly prohibited
-    * Proprietary and confidential
-    *
-    * Written by Tian Permana <hello@tianpermana.com>, January 2014
-    *
-    */
-
   var uuid = require('node-uuid')
 
   module.exports = function(){
