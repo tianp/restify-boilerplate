6c1776d3b8575d707a8bbc14a680a432b2a6c649
Tian Permana <hello@tianpermana.com>
2015-01-08 22:36:23 +0700

    pm2 ready

---
+++ src/routes/users/delete.js
@@ -0,0 +1,11 @@
+(function(){
+
+  var _ = require('underscore'),
+  moment = require('moment'),
+  Controllers = require('./../../controllers')
+
+  module.exports = function( request, response, next ) {
+
+  }
+
+})()
